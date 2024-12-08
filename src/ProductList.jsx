132e97106de91eef1ae7a3b6e01599c985ab84f5
xeeble <49564459+xeeble@users.noreply.github.com>
2024-12-08 21:26:51 +0100

    Update ProductList.jsx
    updated home link

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -271,7 +271,7 @@ function ProductList(props) {
             <div className="tag">
                <div style={{cursor:"pointer"}} onClick={props.toLanding} className="luxury">
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-               <a href="/" style={{textDecoration:'none'}}>
+               <a style={{textDecoration:'none'}}>
                         <div>
                     <h3 style={{color:'white'}}>Paul's Plants</h3>
                     <i style={{color:'white'}}>Where Green Meets Serenity</i>
