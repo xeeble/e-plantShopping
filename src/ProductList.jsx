f7031e94816804f3773f3fe8e4dbd5d563241de4
xeeble <49564459+xeeble@users.noreply.github.com>
2024-12-08 21:33:16 +0100

    Update ProductList.jsx
    updated navigation

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -269,7 +269,7 @@ function ProductList(props) {
         <div>
              <div className="navbar" style={styleObj}>
             <div className="tag">
-               <div style={{cursor:"pointer"}} onClick={props.toLanding} className="luxury">
+               <div style={{cursor:"pointer"}} onClick={props.landingPage} className="luxury">
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                <a style={{textDecoration:'none'}}>
                         <div>
